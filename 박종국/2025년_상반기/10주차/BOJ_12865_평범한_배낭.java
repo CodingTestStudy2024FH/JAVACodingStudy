327056661ba015bc9dae263b067cb7acb70eaed1
jongkookE <whdrnrdl789@naver.com>
2025-04-21 18:35:31 +0900

    [BOJ12865]평범한_배낭

---
+++ 박종국/2025년_상반기/10주차/BOJ_12865_평범한_배낭.java
@@ -0,0 +1,24 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_12865_평범한_배낭 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int number = Integer.parseInt(st.nextToken());
+        int maxWeight = Integer.parseInt(st.nextToken());
+        int[] dp = new int[maxWeight+1];
+        for(int n = 0; n < number; n++){
+            st = new StringTokenizer(br.readLine());
+            int weight = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
+
+            for(int d = maxWeight; d >= weight; d--) dp[d] = Math.max(dp[d], dp[d-weight] + value);
+        }
+        System.out.println(dp[maxWeight]);
+    }
+}
